33b1061a145e87d7b19c61e3c76da40b307abc2a
przemo509 <przemo509@gmail.com>
2016-01-31 16:24:28 +0100

    switched from casablanca to boost

--- DistributedFileClient/client.cpp
+++ DistributedFileClient/client.cpp
@@ -1,53 +1,50 @@
-#include <cpprest/http_client.h>
-#include <cpprest/filestream.h>
+//
+// client.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 
-using namespace utility;                    // Common utilities like string conversions
-using namespace web;                        // Common features like URIs.
-using namespace web::http;                  // Common HTTP functionality
-using namespace web::http::client;          // HTTP client features
-using namespace concurrency::streams;       // Asynchronous streams
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
 
-int main(int argc, char* argv[])
+using boost::asio::ip::tcp;
+
+int main()
 {
-	auto fileStream = std::make_shared<ostream>();
-
-	// Open stream to output file.
-	pplx::task<void> requestTask = fstream::open_ostream(U("results.html")).then([=](ostream outFile)
-	{
-		*fileStream = outFile;
-
-		// Create http_client to send the request.
-		http_client client(U("http://www.bing.com/"));
-
-		// Build request URI and start the request.
-		uri_builder builder(U("/search"));
-		builder.append_query(U("q"), U("Casablanca CodePlex"));
-		return client.request(methods::GET, builder.to_string());
-	})
-
-		// Handle response headers arriving.
-		.then([=](http_response response)
-	{
-		printf("Client2 Received response status code:%u\n", response.status_code());
-
-		// Write response body into the file.
-		return response.body().read_to_end(fileStream->streambuf());
-	})
-
-		// Close the file stream.
-		.then([=](size_t)
-	{
-		return fileStream->close();
-	});
-
-	// Wait for all the outstanding I/O to complete and handle any exceptions
 	try
 	{
-		requestTask.wait();
+		boost::asio::io_service io_service;
+
+		tcp::resolver resolver(io_service);
+		tcp::resolver::query query("localhost", "12345");
+		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+		tcp::socket socket(io_service);
+		boost::asio::connect(socket, endpoint_iterator);
+
+		for (;;)
+		{
+			boost::array<char, 128> buf;
+			boost::system::error_code error;
+
+			size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+			if (error == boost::asio::error::eof)
+				break; // Connection closed cleanly by peer.
+			else if (error)
+				throw boost::system::system_error(error); // Some other error.
+
+			std::cout.write(buf.data(), len);
+		}
 	}
-	catch (const std::exception &e)
+	catch (std::exception& e)
 	{
-		printf("Error exception:%s\n", e.what());
+		std::cerr << e.what() << std::endl;
 	}
 
 	system("pause");
