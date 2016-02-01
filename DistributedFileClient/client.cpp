480fd3e1afad947697b56068b745266e0129ac81
przemo509 <przemo509@gmail.com>
2016-02-01 02:07:50 +0100

    sending and receiving data between client and server

--- DistributedFileClient/client.cpp
+++ DistributedFileClient/client.cpp
@@ -1,50 +1,46 @@
-//
-// client.cpp
-// ~~~~~~~~~~
-//
-// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
 #include <iostream>
 #include <boost/array.hpp>
-#include <boost/asio.hpp>
 
-using boost::asio::ip::tcp;
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+
+#include <boost/asio.hpp> // must be ater logging (otherwise you get linker error, don't know why)
+
+static const std::string DELIMETER = "\n";
 
 int main()
 {
+	// global logging filter
+	boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::trace);
+
 	try
 	{
-		boost::asio::io_service io_service;
+		// preparing socket
+		using namespace boost::asio;
+		io_service io_service;
+		ip::tcp::socket socket(io_service);
+		socket.connect(ip::tcp::endpoint(ip::address_v4::from_string("127.0.0.1"), 12345));
 
-		tcp::resolver resolver(io_service);
-		tcp::resolver::query query("localhost", "12345");
-		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+		// sending request
+		std::string request("test request");
+		write(socket, buffer(request + DELIMETER));
 
-		tcp::socket socket(io_service);
-		boost::asio::connect(socket, endpoint_iterator);
-
-		for (;;)
-		{
-			boost::array<char, 128> buf;
-			boost::system::error_code error;
-
-			size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-			if (error == boost::asio::error::eof)
-				break; // Connection closed cleanly by peer.
-			else if (error)
-				throw boost::system::system_error(error); // Some other error.
-
-			std::cout.write(buf.data(), len);
+		// receiving response
+		boost::system::error_code errorCode;
+		boost::asio::streambuf buf;
+		size_t bytesReceived = read_until(socket, buf, DELIMETER, errorCode);
+		if (errorCode != 0) {
+			// TODO handle eof when server gets down
+			BOOST_LOG_TRIVIAL(fatal) << "Exception: [" << errorCode.message() << "]";
 		}
+		std::string response;
+		std::getline(std::istream(&buf), response);
+		BOOST_LOG_TRIVIAL(debug) << "Response received: " << response;
 	}
 	catch (std::exception& e)
 	{
-		std::cerr << e.what() << std::endl;
+		BOOST_LOG_TRIVIAL(fatal) << "Exception: [" << e.what() << "]";
 	}
 
 	system("pause");
