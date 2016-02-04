6ba8c3b33a548e6c92b5ec1fb35f77371895f786
przemo509 <przemo509@gmail.com>
2016-02-04 01:40:58 +0100

    first check errorcode, then bytes received + atoi -> stoi (atoi returns 0 when string is not a number!)

--- DistributedFileClient/network.h
+++ DistributedFileClient/network.h
@@ -13,12 +13,12 @@ using namespace std;
 static const int HEADER_LENGTH = 5;
 
 void assertResponseOk(size_t bytesReceived, int assumedLength, boost::system::error_code& errorCode) {
-	if (bytesReceived != assumedLength) {
-		throw new exception(string("Exception while reading response: unexpected length: " + bytesReceived).c_str());
-	}
 	if (errorCode != 0) {
 		// TODO handle eof when server gets down
 		throw new exception(("Exception while reading response: error: " + errorCode.message()).c_str());
+	}
+	if (bytesReceived != assumedLength) {
+		throw new exception(string("Exception while reading response: unexpected length: " + bytesReceived).c_str());
 	}
 }
 
@@ -30,7 +30,7 @@ string getBufAsString(boost::asio::streambuf& buf) {
 
 int getResponseLength(boost::asio::streambuf& buf) {
 	string header = getBufAsString(buf);
-	int responseLength = atoi(header.c_str());
+	int responseLength = stoi(header.c_str());
 	return responseLength;
 }
 
@@ -59,7 +59,7 @@ string getResponse(server_config& config, string request) {
 		boost::system::error_code errorCode;
 		boost::asio::streambuf buf;
 		BOOST_LOG_TRIVIAL(debug) << "Waiting for response length header...";
-		size_t bytesReceived = read(socket, buf, detail::transfer_exactly_t(HEADER_LENGTH), errorCode);
+		size_t bytesReceived = read(socket, buf, boost::asio::detail::transfer_exactly_t(HEADER_LENGTH), errorCode);
 		assertResponseOk(bytesReceived, HEADER_LENGTH, errorCode);
 		int responseLength = getResponseLength(buf);
 		//buf.consume(bytesReceived); // clear buf
@@ -67,7 +67,7 @@ string getResponse(server_config& config, string request) {
 
 		// receiving response body
 		BOOST_LOG_TRIVIAL(debug) << "Waiting for response body...";
-		bytesReceived = read(socket, buf, detail::transfer_exactly_t(responseLength), errorCode);
+		bytesReceived = read(socket, buf, boost::asio::detail::transfer_exactly_t(responseLength), errorCode);
 		assertResponseOk(bytesReceived, responseLength, errorCode);
 		string response = getBufAsString(buf);
 		BOOST_LOG_TRIVIAL(debug) << "Response body received:\n" << response;
